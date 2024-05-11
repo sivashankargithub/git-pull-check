870694019fdfce86683ca0b9f177effd558e44ff
Sivashankar C <sivashankarchandrappa.sangamone@gmail.com>
2024-05-11 16:08:16 +0530

    git hello

---
+++ src/main/java/rest/HelloWorld.java
@@ -0,0 +1,13 @@
+package rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+	@GetMapping("/hello")
+	public String printHello() {
+		return "Hello Sivashankar World";
+		
+	}
+}
