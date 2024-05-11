de87196a910f57457b5f3cfd73da4dcdd72c6beb
Sivashankar C <144573155+sivashankargithub@users.noreply.github.com>
2024-05-11 16:20:54 +0530

    Update HelloWorld.java

--- src/main/java/rest/HelloWorld.java
+++ src/main/java/rest/HelloWorld.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
 	@GetMapping("/hello")
 	public String printHello() {
+		
 		return "Hello Sivashankar World";
 		
 	}
